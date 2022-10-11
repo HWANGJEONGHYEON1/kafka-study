2301c7ad673cd5dc39975cf172d50988f8e2b59f
hwangjeonghyeon1 <deert202@naver.com>
2022-10-11 13:51:59 +0900

    [study] consumer 실습

---
+++ producer/src/main/java/com/example/producer/custom/SimpleProducer.java
@@ -0,0 +1,35 @@
+package com.example.producer.custom;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+
+    private final static Logger logger = LoggerFactory.getLogger(SimpleProducer.class);
+    private final static String TOPIC_NAME = "test";
+    private final static String BOOTSTRAP_SERVER = "my-kafka:9092";
+
+    public static void main(String[] args) {
+        Properties configs = new Properties();
+        configs.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        configs.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        configs.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, CustomPartitioner.class);
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(configs);
+        int partitionNo = 0;
+        ProducerRecord<String, String> record1 = new ProducerRecord<>(TOPIC_NAME, partitionNo, "Seoul", "서울");
+        ProducerRecord<String, String> record2 = new ProducerRecord<>(TOPIC_NAME, partitionNo, "Pangyo", "판교");
+        producer.send(record1);
+        producer.send(record2);
+        logger.info("{}", record1);
+        producer.flush();
+        producer.close();
+    }
+}
